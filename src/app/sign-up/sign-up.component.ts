ec0b4048a335e32f3f8a350c4d4525b401d5f8f2
MalPro <malshis@yahoo.com>
2023-12-18 17:47:30 +0100

    correction of form validation

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { User } from './user.model';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,37 +7,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-  user: User = new User('', '', '', '');
+  user: User = { firstname: '', lastname: '', email: '', password: '' };
 
-  signupForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.signupForm = this.fb.group({
-      firstName: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(15),
-        ],
-      ],
-      lastName: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(20),
-        ],
-      ],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-    });
-  }
   onSubmit() {
-    if (this.signupForm.valid) {
-      console.log('User submitted:', this.signupForm.value);
-    } else {
-      console.error('Form is invalid. Please check the fields.');
-    }
+    console.log('Form is valid', this.user);
+  }
+  change(e: any) {
+    console.log(e);
   }
 }
